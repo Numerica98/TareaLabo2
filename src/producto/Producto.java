4bd11da5afad2515a22ddab7ad155ec3de79ac2c
Melissa <Melissa@DESKTOP-5II7QNA>
2018-04-06 16:58:37 -0300

    Tarea labo2, solo con clases y atributos

---
+++ src/producto/Producto.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package producto;
+
+/**
+ *
+ * @author Melissa
+ */
+public class Producto {
+    private String nombre, proveedor, telProveedor;
+    private int cantidad;
+    private double costo;
+    
+    public Producto(){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getTelProveedor() {
+        return telProveedor;
+    }
+
+    public void setTelProveedor(String telProveedor) {
+        this.telProveedor = telProveedor;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
